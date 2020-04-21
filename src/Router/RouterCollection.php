a5c22916327c525e9a404d34b63fad1e198be8ff
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-20 22:05:54 -0300

    Passando o Collection do ilumunate como retorno da rota.

--- src/Router/RouterCollection.php
+++ src/Router/RouterCollection.php
@@ -3,6 +3,7 @@
 
 namespace App\Project\Router;
 
+use Illuminate\Support\Collection;
 
 class RouterCollection
 {
@@ -11,7 +12,9 @@ class RouterCollection
     public function add(string $method, string $path, $callback)
     {
         if (!isset($this->collection[$method])) {
-
+            $this->collection[$method] = new Collection();
         }
+
+        $this->collection[$method]->put($path, $callback);
     }
 }
