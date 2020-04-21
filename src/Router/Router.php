1f9d6383a2d2222e3be0fa13f5d42322604d26f3
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-20 22:34:54 -0300

    Adicionando mais coleções de rotas

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,11 +4,28 @@ namespace App\Project\Router;
 
 class Router
 {
+    private $collection;
+
     /**
      * Router constructor.
      */
     public function __construct()
     {
-        echo 'create a framework with php';
+        $this->collection = new RouterCollection();
+    }
+
+    public function get($path, $fn)
+    {
+        $this->request( 'GET', $path, $fn);
+    }
+
+    public function post($path, $fn)
+    {
+        $this->request('POST', $path, $fn);
+    }
+
+    public function request($method, $path, $fn)
+    {
+        $this->collection->add($method, $path, $fn);
     }
 }
