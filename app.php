8a882b65a1ee3504955d327378e480bcb0be4cc1
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-22 21:54:41 -0300

    aplicando DI nas rotas criadas

--- app.php
+++ app.php
@@ -2,14 +2,23 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Project\Router\Router;
+use App\Project\DI\Resolver;
+
 $path_info = $_SERVER['PATH_INFO'] ?? '/';
 $request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
-$router = new \App\Project\Router\Router($path_info, $request_method);
+$router = new Router($path_info, $request_method);
 
 $router->get('/hello', function (){
     return 'php small framework';
 });
 
 $route = $router->run();
-echo $route['callback']();
+
+$data = (new Resolver)->method($route['callback'], [
+    'params' => $route['params'],
+]);
+
+// echo $route['callback']();
+echo $data;
