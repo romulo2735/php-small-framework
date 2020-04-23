189779bd39b59a55c1109a6feaf799b3e5a209e2
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-22 21:17:31 -0300

    executando rotas dinamicas

--- app.php
+++ app.php
@@ -2,8 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$router = new \App\Project\Router\Router();
+$path_info = $_SERVER['PATH_INFO'] ?? '/';
+$request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+$router = new \App\Project\Router\Router($path_info, $request_method);
 
 $router->get('/hello', function (){
     return 'php small framework';
 });
+
+$route = $router->run();
+echo $route['callback']();
