c6ea25d27d7f55f9202dca322895a17f5037e283
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-23 20:49:43 -0300

    desacoplando a configuração do arquivo de inicialização

--- app.php
+++ app.php
@@ -2,26 +2,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Project\Router\Router;
-use App\Project\DI\Resolver;
-use App\Project\Render\PHPRender;
-
-$path_info = $_SERVER['PATH_INFO'] ?? '/';
-$request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
-
-$router = new Router($path_info, $request_method);
+$app = new \App\Project\App();
+$app->setRender(new \App\Project\Render\PHPRender());
 
 require __DIR__.'/routes.php';
 
-$route = $router->run();
-
-$data = (new Resolver)->method($route['callback'], [
-    'params' => $route['params'],
-]);
-
-$render = new PHPRender();
-$render->setData($data);
-$render->run();
-
-// echo $route['callback']();
-echo $data;
+$app->run();
