75cc93c54509146fe476c96893b96cf97e117523
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-23 20:05:01 -0300

    adicionando renderização de dados

--- app.php
+++ app.php
@@ -4,21 +4,24 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\Project\Router\Router;
 use App\Project\DI\Resolver;
+use App\Project\Render\PHPRender;
 
 $path_info = $_SERVER['PATH_INFO'] ?? '/';
 $request_method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
 $router = new Router($path_info, $request_method);
 
-$router->get('/hello', function (){
-    return 'php small framework';
-});
+require __DIR__.'/routes.php';
 
 $route = $router->run();
 
 $data = (new Resolver)->method($route['callback'], [
     'params' => $route['params'],
 ]);
+
+$render = new PHPRender();
+$render->setData($data);
+$render->run();
 
 // echo $route['callback']();
 echo $data;
