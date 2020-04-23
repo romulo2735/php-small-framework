964d6f9d9fe292b9b59f33fea414ddce0e09f40d
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-23 20:50:20 -0300

    routes

--- routes.php
+++ routes.php
@@ -1,5 +1,9 @@
 <?php
 
-$router->get('/hello', function () {
+$app->get('/hello', function ($params) {
     return 'php small framework';
+});
+
+$app->get('/nome/{name}', function ($params) {
+    return "Recebendo parametros: {$params[1]}";
 });
